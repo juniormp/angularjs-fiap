7f038141781ee2f316968d938da660dcd3397cce
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2017-09-15 00:17:23 -0300

    Correção em msg e limpar form apos cadastro

--- js/controllers/CadastroCtrl.js
+++ js/controllers/CadastroCtrl.js
@@ -9,7 +9,13 @@ app.controller('CadastroCtrl', function ($scope, $location, cadastroServices, to
 	$scope.salvar = function (empresa) {
 		cadastroServices.salvar(empresa, function (response) {
 			console.log(response)
-			if (response.status == 200) {
+			if (response.status == 201) {
+				$scope.empresa = {
+					"nome": "",
+					"login": "",
+					"senha": ""
+				};
+				$scope.cadastro.$setPristine()
 				toastr.success('Cadastro realizado com sucesso', 'Serazo');
 			} else {
 				toastr.error('Erro ao efetuar cadastro', 'Serazo');
